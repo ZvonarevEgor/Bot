e2afbcd2cc5d3472d0a669b2c01a7518f266c2cb
tujhxbu1996 <zvonarev96@mail.ru>
2019-04-10 17:08:36 +0300

    stable version

---
+++ flask_app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, json
+from messageHandler import *
+# -*- coding: utf-8 -*-
+
+token = 'token'
+confirmation_token = 'conf_token'
+
+command_list = [CommandParserHH(['работа', 'hh', 'head', 'hunter', 'парсер',
+                                 'вакансии'], 'найду вакансии на HeadHunter'),
+                Wiki(['википедия', 'вики'], 'найду в википедии \
+                                                   интересующий тебя вопрос')]
+
+handler = Handler(command_list)
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def processing():
+    data = json.loads(request.data)
+    if 'type' not in data.keys():
+        return 'not vk'
+    if data['type'] == 'confirmation':
+        return confirmation_token
+    elif data['type'] == 'message_new':
+        handler.handle(data['object'], token)
+        return "ok"
